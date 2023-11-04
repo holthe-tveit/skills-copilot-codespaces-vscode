7fae3794219f8f845d72ba1a75ec239921e6329c
Tomas Eilev Holthe-Tveit <tomas@holthe-tveit.no>
2023-11-04 17:20:05 +0000

    Copilot third commit 2

--- comments.js
+++ comments.js
@@ -1,9 +1,27 @@
 // Create Web Server
-// npm install express --save
-// npm install body-parser --save
-// npm install cookie-parser --save
-// npm install express-session --save
-// npm install multer --save
+// Step 1: Create a server object
+// Step 2: Create a port to listen for requests
+// Step 3: Write a response to the client
+// Step 4: Send the response to the client
+// Step 5: Log the request
+// Step 6: Start the server
+// Step 7: Test the server
 
-var express = require('express');
-var bodyParser = require('body-parser');
+// Step 1: Create a server object
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+  // Step 3: Write a response to the client
+  // res.write('Hello World from Node.js');
+  // res.end();
+
+  // Step 4: Send the response to the client
+  // res.end('Hello World from Node.js');
+
+  // Step 5: Log the request
+  console.log(req.url, req.method, req.headers);
+
+  // Step 6: Start the server
+  // Step 7: Test the server
+  // res.end
