7c8af3aec280cc525418e153b88dee329a56aa71
Tomas Eilev Holthe-Tveit <tomas@holthe-tveit.no>
2023-11-04 17:14:53 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,10 @@
+function skillsMember() {
+    var skills = document.getElementById("skills");
+    var skillsMember = document.getElementById("skillsMember");
+    var skillsMember2 = document.getElementById("skillsMember2");
+    var skillsMember3 = document.getElementById("skillsMember3");
+    skills.style.display = "block";
+    skillsMember.style.display = "block";
+    skillsMember2.style.display = "none";
+    skillsMember3.style.display = "none";
+}
